a008b9b7658a15a51274cf3c92c29a8bbb37f009
Maximilian Seitzer <maximilian.seitzer@fau.de>
2016-11-02 23:43:28 +0100

    Add notary charter data download script

---
+++ src/data/notary_charters/dl_notary_charters.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+"""Script to download images from monasterium.net from xml databases"""
+
+import sys, os
+import argparse
+import subprocess
+import urllib.parse
+import xml.etree.ElementTree as ET
+
+parser = argparse.ArgumentParser(description='download images from monasterium')
+parser.add_argument('-o', '--output', dest='target_folder',
+                    help='Folder to save images in', default='.')
+parser.add_argument('--only-index', action='store_true',
+                    dest='only_index', default=False,
+                    help='Only generate index file, do not download images')
+parser.add_argument('input_files', nargs='+', help='Input XML files')
+
+def parse_xml(path):
+    tree = ET.parse(path)
+    root = tree.getroot()
+
+    charters = []
+    for charter in root.iter('charter'):
+        attribs = {}
+        for child in charter:
+            attribs[child.tag] = child.text
+        charters.append(attribs)
+    return charters
+
+
+def download_images(folder, images):
+    for image in images:
+        subprocess.call(['wget', '-P', folder, image])
+
+
+def construct_index(f, folder, charters):
+    for charter in charters:
+        url = urllib.parse.urlparse(charter['imageFile'])
+        image_name = os.path.basename(url.path)
+        f.write('{}/{};{}\n'.format(folder, image_name, charter['date']))
+
+
+def main(arguments):
+    args = parser.parse_args(arguments)
+    
+    base_folder = os.path.normpath(args.target_folder)
+    if not os.path.exists(base_folder):
+        print('Target folder {} does not exist'.format(base_folder))
+        return
+
+    for file in args.input_files:
+        charters = parse_xml(file)
+
+        image_urls = [charter['imageFile'] for charter in charters]
+
+        folder_name = os.path.splitext(os.path.basename(file))[0]
+        folder_path = '{}/{}'.format(base_folder, folder_name)
+
+        if not os.path.exists(folder_path):
+            os.mkdir(folder_path)
+
+        csv_file = '{}/{}.csv'.format(base_folder, folder_name)
+        with open(csv_file, 'w') as f:
+            construct_index(f, folder_name, charters)
+
+        if not args.only_index:
+            download_images(folder_path, image_urls)
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
