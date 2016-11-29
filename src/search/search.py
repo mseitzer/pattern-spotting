fbe17385aa511e671e08264bb12a99dcc54621b0
Maximilian Seitzer <maximilian.seitzer@fau.de>
2016-11-29 18:48:32 +0100

    Implement querying for top_n results properly

--- src/search/search.py
+++ src/search/search.py
@@ -8,26 +8,48 @@ from ..features import compute_representation, convert_image, load_image
 from .search_model import SearchModel
 
 
+def _descending_argsort(array, k):
+    """Return indices that index the highest k values in an array"""
+    indices = np.argpartition(array, -k)[-k:]
+    indices = indices[np.argsort(array[indices])]  # Sort indices
+    return indices[::-1]
+
+
+def _ascending_argsort(array, k):
+    """Return indices that index the lowest k values in an array"""
+    indices = np.argpartition(array, k-1)[:k]
+    indices = indices[np.argsort(array[indices])]  # Sort indices
+    return indices
+
+
 def query(query_features, feature_store, top_n=0):
-    """Return a list of indices of the most similar features"""
+    """Query stored features for similarity against a passed feature
+
+    Args:
+        query_features: Feature to query for, shape (dim,)
+        feature_store: 2D-array of n features to compare to, shape (n, dim)
+        top_n:  if zero, return all results in descending order
+                if positive, return only the best top_n results
+                if negative, return only the worst top_n results
+
+    Returns:
+        (indices, similarities), where indices is an array of top_n 
+        indices of entries in the feature_store sorted by decreasing 
+        similarity, and similarities contains the corresponding 
+        similarity value for each entry.
+    """
     query_features = np.expand_dims(query_features, axis=0)
 
     # Cosine similarity measure
     similarity = feature_store.dot(query_features.T).flatten()
 
-    if top_n == 0:
-        k = -len(similarity)+1  # Every feature
-    elif top_n > 0:
-        k = -top_n+1  # Only best top_n
-    else:
-        k = -top_n  # Only worst top_n
+    k = min(len(feature_store), abs(top_n))
+    if top_n >= 0:  # Best top_n features
+        indices = _descending_argsort(similarity, k)
+    else:  # Worst top_n features
+        indices = _ascending_argsort(similarity, k)
     
-    # TODO: return only top_n results
-    indices = np.argpartition(similarity, k)
-    indices = indices[np.argsort(similarity[indices])]  # Sort indices
-
-    top_similarities = similarity[indices]
-    return indices, top_similarities
+    return indices, similarity[indices]
 
 
 def search_roi(search_model, image, roi=None, top_n=0):
@@ -37,10 +59,10 @@ def search_roi(search_model, image, roi=None, top_n=0):
         image: RGB PIL image to take roi of
         roi: bounding box in the form of (left, upper, right, lower)
         feature_store: features to search on
-        top_n: how many query results to return
+        top_n: how many query results to return (see query())
 
     Returns:
-        (indices, similarities), where indices is a list of top_n 
+        (indices, similarities), where indices is an array of top_n 
         indices of entries in the feature_store sorted by decreasing 
         similarity, and similarities contains the corresponding 
         similarity value for each entry.
