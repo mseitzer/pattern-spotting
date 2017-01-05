57dd57bfb30549533c5d4dcb633b3e80795da369
Maximilian Seitzer <maximilian.seitzer@fau.de>
2017-01-05 19:10:56 +0100

    Add test runner script

---
+++ cmd/test_runner.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""Script to execute all of the projects unit tests"""
+
+import os
+import subprocess
+import sys
+
+# Defines all unit test scripts
+tests = [
+    'src/features/test_extract.py'
+]
+
+def main(args):
+    results = []
+    for test in tests:
+        cwd = os.path.join('..', os.path.dirname(test))
+        file = os.path.basename(test)
+
+        res = subprocess.call(["python", file], cwd=cwd)
+        results.append(res)
+
+    print('Test summary: ')
+    num_tests_passed = 0
+    for test, res in zip(tests, results):
+        if res == 0:
+            res_str = 'PASS'
+            num_tests_passed += 1
+        else:
+            res_str = 'FAIL'
+        print('{}: {}'.format(test, res_str))
+    print('Passed {}/{} tests'.format(num_tests_passed, len(tests)))
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
