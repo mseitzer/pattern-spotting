df62f387ee6a9e9bfc0a446dfc1d397a12e3c557
Maximilian Seitzer <maximilian.seitzer@fau.de>
2017-01-20 15:01:30 +0100

    Add output option to query script

--- cmd/query.py
+++ cmd/query.py
@@ -2,15 +2,14 @@
 import os
 import sys
 import argparse
+from os.path import join, basename
 
-from PIL import Image
+from PIL import Image, ImageDraw
 
 # Path hack to be able to import from sibling directory
 sys.path.append(os.path.abspath(os.path.split(os.path.realpath(__file__))[0]
                                 + '/..'))
-
 from src.search import SearchModel, search_roi
-
 
 parser = argparse.ArgumentParser(description=
                                  'Query a database for similar images')
@@ -20,6 +19,11 @@ parser.add_argument('--features', required=True,
                     help='Path to features to use')
 parser.add_argument('--model', required=True, 
                     help='Name or path of model to use')
+parser.add_argument('--output', 
+                    help='Folder to output the found images '
+                    'with bounding box annotations')
+parser.add_argument('--image-dir',
+                    help='Image directory. Required with the output option.')
 parser.add_argument('-n', dest='top_n', type=int, default=5, 
                     help='How many search results to display')
 parser.add_argument('--bbox', type=int, nargs=4, default=None,
@@ -36,6 +40,10 @@ def main(args):
     else:
         bbox = None
 
+    if args.output and not args.image_dir:
+        print('Image directory required with option output')
+        return
+
     query_images = []
     for image_path in args.images:
         if os.path.exists(image_path):
@@ -51,14 +59,26 @@ def main(args):
 
         results, similarities, bboxes = search_roi(search_model, 
                                                    image, 
-                                                   roi=bbox,
-                                                   top_n=args.top_n)
+                                                   roi=bbox)
 
-        print('Top {} results for query image {}'.format(len(results), 
-                                                         image_path))
-        for result, similarity, bbox in zip(results, similarities, bboxes):
+        N = args.top_n
+        print('Top {} results for query image {}'.format(N, image_path))
+        for result, similarity, bbox in zip(results[:N], 
+                                            similarities[:N], 
+                                            bboxes[:N]):
             result_path = search_model.get_metadata(result)['image']
+            
             print('{}\t{:.4f}\t{}'.format(result_path, similarity, bbox))
+
+            if args.output:
+                image = Image.open(join(args.image_dir, result_path))
+                image.convert('RGB')
+                draw = ImageDraw.Draw(image)
+                draw.rectangle(bbox, outline=(255, 0, 0))
+                path = join(args.output, '{}_{:.4f}_{}'.format(
+                    basename(image_path), similarity, basename(result_path)
+                ))
+                image.save(path)
 
 if __name__ == '__main__':
     main(sys.argv[1:])
