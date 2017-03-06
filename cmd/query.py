e08491b14f14d30b4e28ba15d8dbf167a464a14f
Maximilian Seitzer <maximilian.seitzer@fau.de>
2017-03-06 23:44:14 +0100

    Simplify query script

--- cmd/query.py
+++ cmd/query.py
@@ -2,6 +2,7 @@
 import os
 import sys
 import argparse
+from timeit import default_timer as timer
 from os.path import join, basename
 
 from PIL import Image, ImageDraw
@@ -9,6 +10,7 @@ from PIL import Image, ImageDraw
 # Path hack to be able to import from sibling directory
 sys.path.append(os.path.abspath(os.path.split(os.path.realpath(__file__))[0]
                                 + '/..'))
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 from src.util import convert_image, crop_image
 from src.search import SearchModel, search
 
@@ -33,14 +35,21 @@ parser.add_argument('--bbox', type=int, nargs=4, default=None,
 parser.add_argument('images', nargs='+', 
                     help='One or more images to query for')
 
+
+def draw_bbox_and_save(image_path, target_path, bbox):
+    with Image.open(image_path) as image:
+        image.convert('RGB')
+        draw = ImageDraw.Draw(image)
+        draw.rectangle(bbox, outline=(255, 0, 0))
+        image.save(target_path)
+
+
 def main(args):
     args = parser.parse_args(args)
 
     if args.bbox:
-        bbox = tuple(args.bbox)
-    else:
-        bbox = None
-
+        args.bbox = tuple(args.bbox)
+    
     if args.output and not args.image_dir:
         print('Image directory required with option output')
         return
@@ -55,31 +64,32 @@ def main(args):
     search_model = SearchModel(args.model, args.features, args.database)
 
     for image_path in query_images:
-        image = Image.open(image_path).convert('RGB')
-        if bbox:
-            image = crop_image(image, bbox)
-        image = convert_image(image)
+        with Image.open(image_path) as image:
+            image = image.convert('RGB')
+            if args.bbox:
+                image = crop_image(image, args.bbox)
+            query = convert_image(image)
         
-        results, similarities, bboxes = search(search_model, image, top_n=0)
+        start_time = timer()
+        results, similarities, bboxes = search(search_model, query, 
+                                               top_n=args.top_n)
+        end_time = timer()
 
-        N = args.top_n
-        print('Top {} results for query image {}'.format(N, image_path))
-        for result, similarity, bbox in zip(results[:N], 
-                                            similarities[:N], 
-                                            bboxes[:N]):
+        print('Search took {:.6f} seconds'.format(end_time-start_time))
+        print('Top {} results for query image {}'.format(args.top_n, 
+                                                         image_path))
+        for result, similarity, bbox in zip(results, similarities, bboxes):
             result_path = search_model.get_metadata(result)['image']
 
             print('{}\t{:.4f}\t{}'.format(result_path, similarity, bbox))
 
             if args.output:
-                image = Image.open(join(args.image_dir, result_path))
-                image.convert('RGB')
-                draw = ImageDraw.Draw(image)
-                draw.rectangle(bbox, outline=(255, 0, 0))
-                path = join(args.output, '{}_{:.4f}_{}'.format(
-                    basename(image_path), similarity, basename(result_path)
+                result_path = join(args.image_dir, result_path)
+                target_path = join(args.output, '{}_{:.4f}_{}'.format(
+                    basename(query_path), similarity, basename(result_path)
                 ))
-                image.save(path)
+                draw_bbox_and_save(result_path, target_path, bbox)
+                
 
 if __name__ == '__main__':
     main(sys.argv[1:])
